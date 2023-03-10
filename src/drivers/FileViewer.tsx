e181fd874d4fdcd4058be56ff0510d60bc4eb22e
Rostislav Berezhnoy <berezhnoy.rostislav@gmail.com>
2023-03-10 22:55:29 +0300

    fix: text typo

--- src/drivers/FileViewer.tsx
+++ src/drivers/FileViewer.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { DocxViwer } from './components/DocsViwer'
+import { DocxViwer } from './components/DocsViewer'
 import { PdfViewer } from './components/PdfViewer'
 import { isDocsType, isPdfType } from './utils/guards'
 import { FileViewerProps } from './types'
