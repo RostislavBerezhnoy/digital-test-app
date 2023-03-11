5683696637297333c23069273d45b7273baf71bc
Rostislav Berezhnoy <berezhnoy.rostislav@gmail.com>
2023-03-11 20:54:51 +0300

    fix: remove unnecessary exports

--- src/drivers/index.tsx
+++ src/drivers/index.tsx
@@ -1,6 +1,3 @@
 export * from './FileViewer'
 export * from './types'
-export * from './components/DocsViewer'
-export * from './components/ImageViewer'
-export * from './components/PdfViewer'
 export * from './utils/guards'
